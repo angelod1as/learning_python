c276f7057235ed666b200bdb65f2f2b38f450893
Angelo Dias <oiangelodias@gmail.com>
2024-11-23 23:37:12 -0300

    organizing and fixing

--- pysnake/snake.py
+++ pysnake/snake.py
@@ -7,51 +7,73 @@ def game_loop(window, game_speed):
     # Setup inicial
     curses.curs_set(0)
     snake = [
+        [12, 15],
+        [11, 15],
         [10, 15],
         [9, 15],
         [8, 15],
         [7, 15],
+        [6, 15],
     ]
     fruit = get_new_fruit(window=window)
     current_direction = curses.KEY_DOWN
     snake_ate_fruit = False
     score = 0
 
+    # Loop do jogo
     while True:
+        # Desenhar elementos
         draw_screen(window=window)
         draw_snake(snake=snake, window=window)
         draw_actor(actor=fruit, window=window, char=curses.ACS_DIAMOND)
+        # Pegar input do usuário
         direction = get_new_direction(window=window, timeout=game_speed)
-        if direction == 27:
-            break
         if direction is None:
             direction = current_direction
-        if direction_is_opposite(direction=direction, current_direction=current_direction):
+        elif direction_is_opposite(direction=direction, current_direction=current_direction):
             direction = current_direction
         move_snake(snake=snake, direction=direction,
                    snake_ate_fruit=snake_ate_fruit)
-        if actor_hit_border(actor=snake[0], window=window):
+        # Checar final de jogo
+        if snake_hit_border(snake=snake, window=window):
             break
         if snake_hit_itself(snake=snake):
             break
+        # Checar colisão
         if snake_hit_fruit(snake=snake, fruit=fruit):
+            score += 1
             snake_ate_fruit = True
             fruit = get_new_fruit(window=window)
-            score += 1
         else:
             snake_ate_fruit = False
+        # Atualizar direção atual
         current_direction = direction
+
+    # Mensagem ao final do jogo
     finish_game(score=score, window=window)
 
 
 def finish_game(score, window):
-    height, width, = window.getmaxyx()
-    s = f"Você perdeu! Coletou {score} frutas!"
-    y = int(height / 2)
+    height, width = window.getmaxyx()
+    s = f'Você perdeu! Coletou {score} frutas!'
     x = int((width - len(s)) / 2)
+    y = int(height / 2)
     window.addstr(y, x, s)
     window.refresh()
     time.sleep(2)
+
+
+def get_new_fruit(window):
+    height, width = window.getmaxyx()
+    return [random.randint(1, height-2), random.randint(1, width-2)]
+
+
+def get_new_direction(window, timeout):
+    window.timeout(timeout)
+    direction = window.getch()
+    if direction in [curses.KEY_UP, curses.KEY_LEFT, curses.KEY_DOWN, curses.KEY_RIGHT]:
+        return direction
+    return None
 
 
 def direction_is_opposite(direction, current_direction):
@@ -66,54 +88,10 @@ def direction_is_opposite(direction, current_direction):
             return current_direction == curses.KEY_LEFT
 
 
-def get_new_fruit(window):
-    height, width = window.getmaxyx()
-    return [random.randint(1, height-2), random.randint(1, width-2)]
-
-
-def snake_hit_border(snake, window):
-    head = snake[0]
-    return actor_hit_border(actor=head, window=window)
-
-
-def snake_hit_fruit(snake, fruit):
-    return fruit in snake
-
-
-def snake_hit_itself(snake):
-    head = snake[0]
-    body = snake[1:]
-    return head in body
-
-
-def draw_screen(window):
-    window.clear()
-    window.border(0)
-
-
-def draw_snake(snake, window):
-    head = snake[0]
-    draw_actor(actor=head, window=window, char="@")
-    body = snake[1:]
-    for body_part in body:
-        draw_actor(actor=body_part, window=window, char="s")
-
-
-def draw_actor(actor, window, char):
-    window.addch(actor[0], actor[1], char)
-
-
-def get_new_direction(window, timeout):
-    window.timeout(timeout)
-    direction = window.getch()
-    if direction in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT, 27]:
-        return direction
-
-
 def move_snake(snake, direction, snake_ate_fruit):
     head = snake[0].copy()
-    move_actor(actor=head, direction=direction)
     snake.insert(0, head)
+    move_actor(actor=head, direction=direction)
     if not snake_ate_fruit:
         snake.pop()
 
@@ -130,9 +108,47 @@ def move_actor(actor, direction):
             actor[1] += 1
 
 
+def snake_hit_border(snake, window):
+    head = snake[0]
+    return actor_hit_border(actor=head, window=window)
+
+
 def actor_hit_border(actor, window):
     height, width = window.getmaxyx()
-    return actor[0] <= 0 or actor[0] >= height-1 or actor[1] <= 0 or actor[1] >= width-1
+    # EIXO VERTICAL
+    if (actor[0] <= 0) or (actor[0] >= (height - 1)):
+        return True
+    # EIXO HORIZONTAL
+    if (actor[1] <= 0) or (actor[1] >= (width - 1)):
+        return True
+    return False
+
+
+def snake_hit_itself(snake):
+    head = snake[0]
+    body = snake[1:]
+    return head in body
+
+
+def snake_hit_fruit(snake, fruit):
+    return fruit in snake
+
+
+def draw_screen(window):
+    window.clear()
+    window.border(0)
+
+
+def draw_snake(snake, window):
+    head = snake[0]
+    body = snake[1:]
+    draw_actor(actor=head, window=window, char="@")
+    for body_part in body:
+        draw_actor(actor=body_part, window=window, char="s")
+
+
+def draw_actor(actor, window, char):
+    window.addch(actor[0], actor[1], char)
 
 
 def select_difficulty():
@@ -144,7 +160,7 @@ def select_difficulty():
         '5': 35,
     }
     while True:
-        answer = input('Selecione a dificuldade de 1 a 5:\n')
+        answer = input('Selecione a dificuldade de 1 a 5:')
         game_speed = difficulty.get(answer)
         if game_speed is not None:
             return game_speed
@@ -153,4 +169,3 @@ def select_difficulty():
 
 if __name__ == '__main__':
     curses.wrapper(game_loop, game_speed=select_difficulty())
-    print('PERDEU!!!')
