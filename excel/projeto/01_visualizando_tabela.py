2c794ea244e9f837485a4e544f07a476b9e8e8a5
Angelo Dias <angelo.dias@appnovation.com>
2025-01-21 15:15:32 -0300

    add streamlit

---
+++ excel/projeto/01_visualizando_tabela.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from pathlib import Path
+import pandas as pd
+
+pasta_datasets = Path(__file__).parent.parent / 'datasets'
+caminho_vendas = pasta_datasets / 'vendas.csv'
+
+df_vendas = pd.read_csv(caminho_vendas, sep=';', decimal=',')
+
+st.dataframe(df_vendas)
