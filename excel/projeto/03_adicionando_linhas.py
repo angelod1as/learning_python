10d13b9cf2ff5fe84575e76a80409497ab410656
Angelo Dias <angelo.dias@appnovation.com>
2025-01-21 16:24:37 -0300

    Fix project

--- excel/projeto/03_adicionando_linhas.py
+++ excel/projeto/03_adicionando_linhas.py
@@ -5,7 +5,9 @@ import streamlit as st
 import pandas as pd
 
 pasta_datasets = Path(__file__).parent.parent / "datasets"
-df_vendas = pd.read_csv(pasta_datasets / "vendas.csv", decimal=",", sep=";")
+df_vendas = pd.read_csv(
+    pasta_datasets / "vendas.csv", decimal=",", sep=";", index_col=0
+)
 df_filiais = pd.read_csv(pasta_datasets / "filiais.csv", decimal=",", sep=";")
 df_produtos = pd.read_csv(pasta_datasets / "produtos.csv", decimal=",", sep=";")
 
@@ -25,10 +27,11 @@ produto_selecionado = st.sidebar.selectbox("Selecione o produto:", lista_produto
 nome_cliente = st.sidebar.text_input("Nome do cliente")
 
 genero_selecionado = st.sidebar.selectbox(
-    "Gênero do cliente", ["Masculino", "Feminino"]
+    "Genêro do cliente:", ["masculino", "feminino"]
 )
+
 forma_pag_selecionado = st.sidebar.selectbox(
-    "Forma de pagamento", ["Crédito", "Pix", "Boleto"]
+    "Forma de pagamento", ["credito", "pix", "boleto"]
 )
 
 if st.sidebar.button("Adicionar nova venda"):
@@ -45,7 +48,4 @@ if st.sidebar.button("Adicionar nova venda"):
     df_vendas.to_csv(pasta_datasets / "vendas.csv", decimal=",", sep=";")
     st.success("Venda adicionada")
 
-
-st.dataframe(df_vendas)
-st.dataframe(df_filiais)
-st.dataframe(df_produtos)
+st.dataframe(df_vendas, height=800)
