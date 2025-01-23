7368fd02c71a02ff8f85357940565491d0119ec2
Angelo Dias <oiangelodias@gmail.com>
2025-01-23 11:25:32 -0300

    format

--- excel/projeto/01_visualizando_tabela.py
+++ excel/projeto/01_visualizando_tabela.py
@@ -1,10 +1,11 @@
-import streamlit as st
 from pathlib import Path
+
+import streamlit as st
 import pandas as pd
 
-pasta_datasets = Path(__file__).parent.parent / 'datasets'
-caminho_vendas = pasta_datasets / 'vendas.csv'
+pasta_datasets = Path(__file__).parent.parent / "datasets"
+caminho_vendas = pasta_datasets / "vendas.csv"
 
-df_vendas = pd.read_csv(caminho_vendas, sep=';', decimal=',')
+df_vendas = pd.read_csv(caminho_vendas, sep=";", decimal=",")
 
 st.dataframe(df_vendas)
